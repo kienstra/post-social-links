25d8e8a8625776b44482d19d9a5dcb397092db17
Ryan Kienstra <kienstraryan@gmail.com>
2022-10-03 00:35:19 -0500

    Add a zip job to CircleCI

--- .circleci/dynamic/index.ts
+++ .circleci/dynamic/index.ts
@@ -5,18 +5,31 @@ const config = new CircleCI.Config();
 const workflow = new CircleCI.Workflow("test-lint");
 config.addWorkflow(workflow);
 
-
-const lint = new CircleCI.Job(
-  "php-lint",
-  new CircleCI.executors.DockerExecutor("cimg/php:8.0", "large"),
-  [
-    new CircleCI.commands.Checkout(),
-    new CircleCI.commands.Run({
-      command: "composer i && composer lint",
-    }),
-  ]
-)
-config.addJob(lint);
-workflow.addJob(lint);
+[
+  new CircleCI.Job(
+    "php-lint",
+    new CircleCI.executors.DockerExecutor("cimg/php:8.0", "large"),
+    [
+      new CircleCI.commands.Checkout(),
+      new CircleCI.commands.Run({
+        command: "composer i && composer lint",
+      }),
+    ]
+  ),
+  new CircleCI.Job(
+    "zip",
+    new CircleCI.executors.DockerExecutor("cimg/php:8.0"),
+    [
+      new CircleCI.commands.Checkout(),
+      new CircleCI.commands.Run({ command: "composer zip" }),
+      new CircleCI.commands.StoreArtifacts({
+        path: "adapter-gravity-add-on.zip",
+      }),
+    ]
+  ),
+].forEach((job) => {
+  config.addJob(job);
+  workflow.addJob(job);
+});
 
 fs.writeFile("./dynamicConfig.yml", config.stringify(), () => {});
