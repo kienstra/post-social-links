da21749aa8576deac9fd2cab35f71559d449f235
Ryan Kienstra <kienstraryan@gmail.com>
2022-10-07 18:12:13 -0500

    Use the common config, instead of the hard-coded one

--- .circleci/dynamic/index.ts
+++ .circleci/dynamic/index.ts
@@ -1,35 +1,3 @@
-import * as fs from "fs";
-import CircleCI from "@circleci/circleci-config-sdk";
+import { createConfig, JobNames } from "@getlocalci/create-config";
 
-const config = new CircleCI.Config();
-const workflow = new CircleCI.Workflow("test-lint");
-config.addWorkflow(workflow);
-
-[
-  new CircleCI.Job(
-    "php-lint",
-    new CircleCI.executors.DockerExecutor("cimg/php:8.0", "large"),
-    [
-      new CircleCI.commands.Checkout(),
-      new CircleCI.commands.Run({
-        command: "composer i && composer lint",
-      }),
-    ]
-  ),
-  new CircleCI.Job(
-    "zip",
-    new CircleCI.executors.DockerExecutor("cimg/php:8.0"),
-    [
-      new CircleCI.commands.Checkout(),
-      new CircleCI.commands.Run({ command: "composer zip" }),
-      new CircleCI.commands.StoreArtifacts({
-        path: "post-social-links.zip",
-      }),
-    ]
-  ),
-].forEach((job) => {
-  config.addJob(job);
-  workflow.addJob(job);
-});
-
-fs.writeFile("./dynamicConfig.yml", config.stringify(), () => {});
+createConfig(JobNames.PhpLint, JobNames.Zip);
